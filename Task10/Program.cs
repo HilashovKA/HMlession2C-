7d61c29d551d671cbb1fb626ad0c2ad3c65ea474
lavr174 <lavr174@icloud.com>
2022-08-13 19:21:20 +0300

    taskV2

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,8 +1,14 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и 
 //на выходе показывает вторую цифру этого числа.
 
-Console.WriteLine("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите трехзначное число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
 
-string stringnum = Convert.ToString(number);
-Console.WriteLine(stringnum[1]);
+//string stringnum = Convert.ToString(number);
+//Console.WriteLine(stringnum[1]);
+
+
+Console.WriteLine("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = (number / 10 ) % 10;
+Console.WriteLine($"Вторая цифра числа равна {digit}");
