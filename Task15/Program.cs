700c359dafbb9c7b971f432a154a9eeaff113c80
lavr174 <lavr174@icloud.com>
2022-08-13 20:04:07 +0300

    taks15TAB

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -4,13 +4,16 @@
 Console.WriteLine("Введите число дня недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-void Week (int dayNumber) {
-    if (dayNumber > 7 || dayNumber < 1){
+void Week (int dayNumber) 
+{
+if (dayNumber > 7 || dayNumber < 1)
+{
 Console.WriteLine("Введите день недели от 1 до 7");
+}
+    else if (dayNumber == 6 || dayNumber == 7) 
+    {
+    Console.WriteLine("Выходной");
     }
-        else if (dayNumber == 6 || dayNumber == 7) {
-        Console.WriteLine("Выходной");
-        }
-            else Console.WriteLine("Не выходной");
+ else Console.WriteLine("Не выходной");
 }
 Week(dayNumber);
